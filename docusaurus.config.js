a08f13efd57f7ea0e12794645e31fc9e216a2471
GPT-Partners <124867543+gpt-partners@users.noreply.github.com>
2023-02-10 09:24:38 +0000

    add deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,12 +6,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'GPT Analytics',
+  tagline: 'Ask your data anything',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.gpt-analytics.gpt-partners.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
