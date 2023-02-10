fd446a06e7098912c51f4803f643509284afe663
GPT-Partners <124867543+gpt-partners@users.noreply.github.com>
2023-02-10 10:02:27 +0000

    change starter text

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            GPT Analytics Tutorial - 5min ⏱️
+            GPT Analytics Tutorial - Start in 5min ⏱️
           </Link>
         </div>
       </div>
